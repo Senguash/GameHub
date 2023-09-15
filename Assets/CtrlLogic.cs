c1575a9c572d6d1e03acc0fef45f9c293d921e76
Senguash <60571135+Senguash@users.noreply.github.com>
2023-09-15 18:56:33 +0200

    Added pause menu

--- Assets/CtrlLogic.cs
+++ Assets/CtrlLogic.cs
@@ -8,19 +8,36 @@ using UnityEngine.UIElements;
 public class CtrlLogic : MonoBehaviour
 {
     VisualElement root;
-
+    VisualElement gameContainer;
+    VisualElement pauseMenu;
+    VisualElement topBar;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Game start");
         root = GetComponent<UIDocument>().rootVisualElement;
+        gameContainer = new VisualElement();
+        gameContainer.style.height = Length.Percent(95);
+        topBar = GenerateTopBar();
+        root.Add(topBar);
+        root.Add(gameContainer);
+        pauseMenu = GeneratePauseMenu();
+        root.Add(pauseMenu);
+        pauseMenu.style.display = DisplayStyle.None;
+        InitMainMenu();
+    }
 
-        VisualElement ve = new VisualElement();
-        ve.style.height = Length.Percent(50);
-        root.Add(ve);
-
+    void InitMainMenu()
+    {
+        topBar.style.display = DisplayStyle.None;
+        pauseMenu.style.display = DisplayStyle.None;
+        gameContainer.Clear();
+        gameContainer.style.display = DisplayStyle.Flex;
         VisualElement selectGamePanel = new VisualElement();
-        root.Add(selectGamePanel);
+        selectGamePanel.style.justifyContent = Justify.Center;
+        selectGamePanel.style.alignItems = Align.Center;
+        //selectGamePanel.style.backgroundColor = Color.green;
+        selectGamePanel.style.height = Length.Percent(100);
 
         Button ludoButton = new Button();
         ludoButton.text = "Play Ludo";
@@ -36,25 +53,79 @@ public class CtrlLogic : MonoBehaviour
         ticTacToeButton.text = "Play Tic Tac Toe";
         ticTacToeButton.clicked += () => { EnterTicTacToe(); };
         selectGamePanel.Add(ticTacToeButton);
+
+        gameContainer.Add(selectGamePanel);
+    }
+
+    VisualElement GenerateTopBar()
+    {
+        VisualElement ve = new VisualElement();
+        ve.style.height = Length.Percent(5);
+        ve.style.flexDirection = FlexDirection.RowReverse;
+        Button b = new Button();
+        b.style.width = Length.Percent(25);
+        b.text = "...";
+        b.clicked += () => { PauseGame(); };
+        ve.Add(b);
+        return ve;
+    }
+
+    VisualElement GeneratePauseMenu()
+    {
+        //gameContainer.style.display = DisplayStyle.None;
+        VisualElement ve = new VisualElement();
+        //ve.style.backgroundColor = Color.gray;
+        ve.style.justifyContent = Justify.Center;
+        ve.style.alignItems = Align.Center;
+        ve.style.height = Length.Percent(100);
+        Button contButton = new Button();
+        contButton.text = "Continue";
+        contButton.clicked += () => { UnpauseGame();  };
+        ve.Add(contButton);
+        Button quitButton = new Button();
+        quitButton.text = "Quit";
+        quitButton.clicked += () => { InitMainMenu();  };
+        ve.Add(quitButton);
+        return ve;
+    }
+
+    void PauseGame()
+    {
+        topBar.style.display = DisplayStyle.None;
+        gameContainer.style.display = DisplayStyle.None;
+        pauseMenu.style.display = DisplayStyle.Flex;
+    }
+
+    void UnpauseGame()
+    {
+        topBar.style.display = DisplayStyle.Flex;
+        gameContainer.style.display = DisplayStyle.Flex;
+        pauseMenu.style.display = DisplayStyle.None;
+    }
+
+    void EnterGame()
+    {
+        topBar.style.display = DisplayStyle.Flex;
+        gameContainer.Clear();
     }
 
     void EnterLudo()
     {
-        root.Clear();
+        EnterGame();
         LudoController ludo = gameObject.AddComponent(typeof(LudoController)) as LudoController;
-        root.Add(ludo.root);
+        gameContainer.Add(ludo.root);
     }
     void EnterSudoku()
     {
-        root.Clear();
+        EnterGame();
         SudokuController sudoku = gameObject.AddComponent(typeof(SudokuController)) as SudokuController;
-        root.Add(sudoku.root);
+        gameContainer.Add(sudoku.root);
     }
     void EnterTicTacToe()
     {
-        root.Clear();
+        EnterGame();
         TicTacToeController tic = gameObject.AddComponent(typeof(TicTacToeController)) as TicTacToeController;
-        root.Add(tic.root);
+        gameContainer.Add(tic.root);
     }
     // Update is called once per frame
     void Update()
