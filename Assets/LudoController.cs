51151b28569a948725b0059154685ac4fe68602a
Thomas Buus Tamborg <thomas.tamborg@live.dk>
2023-10-04 16:41:51 +0200

    Added a lot of stuff

--- Assets/LudoController.cs
+++ Assets/LudoController.cs
@@ -4,36 +4,35 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEditor.Animations;
+using System.Linq;
 
 public class LudoController : Game
 {
 
-
     State newGame = new State("newGame");
-    State coreGame = new State("coreGame");
     State gameEnd = new State("gameEnd");
-    State player1 = new State("player1");
-    State player2 = new State("player2");
-    State player3 = new State("player3");
-    State player4 = new State("player4");
     Command start = new Command("start");
     Command nextPlayer = new Command("nextPlayer");
     // Start is called before the first frame update
+    private readonly Dictionary<int, string> PlayerColors = new Dictionary<int, string>() {
+        {1,"Red" },
+        {2,"Green" },
+        {3,"Blue" },
+        {4,"Yellow" }
+    };
+    
+    private LudoGame game = new LudoGame();
     void Start()
     {
         transitions = new Dictionary<StateTransition, State>()
         {
-            { new StateTransition(newGame, start), player1 },
-            { new StateTransition(player1, nextPlayer), player2 },
-            { new StateTransition(player2, nextPlayer), player3 },
-            { new StateTransition(player3, nextPlayer), player4 },
-            { new StateTransition(player4, nextPlayer), player1 },
+            
         };
 
         newGame.EnterEvent += InitNewGameMenu;
         newGame.LeaveEvent += ClearRoot;
         newGame.LeaveEvent += InitLudoUI;
-        //coreGame.EnterEvent += InitLudoUI;
         SetInitialState(newGame);
 
 
@@ -50,18 +49,51 @@ public class LudoController : Game
     }
     private void InitNewGameMenu()
     {
-        LudoGame game = new LudoGame();
-        root.Add(new Label("Test"));
+        
+        root.Add(new Label("LUDO"));
         Button startGameBtn = new Button();
         Button addPlayerBtn = new Button();
         addPlayerBtn.text = "Add player";
-        addPlayerBtn.clicked += () => { game.AddPlayer("IDK", false); };
-        startGameBtn.text = "Start";
-        startGameBtn.clicked += () => { game.StartGame();
-            MoveNext(start);
+        addPlayerBtn.clicked += () => {
+            int ret = AddPlayer();
+            if (ret == 2)
+            {
+                startGameBtn.SetEnabled(true);
+            }
+            else if(ret == 4)
+            {
+                addPlayerBtn.SetEnabled(false);
+            }
         };
+        startGameBtn.text = "Start";
+        startGameBtn.clicked += () => { StartGame();
+            MoveNext(nextPlayer);
+        };
+        startGameBtn.SetEnabled(false);
         root.Add(addPlayerBtn);
         root.Add(startGameBtn);
+    }
+    private void StartGame()
+    {
+        AddplayerStates();
+    }
+    private void AddplayerStates()
+    {
+        for (int i = 0; i < game.ludoPlayers.Count; i++)
+        {
+            if(i == 0)
+            {
+                transitions.Add(new StateTransition(newGame, nextPlayer), game.ludoPlayers[i].GetPlayerState());
+            }
+            else
+            {
+                transitions.Add(new StateTransition(game.ludoPlayers[i - 1].GetPlayerState(), nextPlayer), game.ludoPlayers[i].GetPlayerState());
+                if (i+1 == game.ludoPlayers.Count)
+                {
+                    transitions.Add(new StateTransition(game.ludoPlayers[i].GetPlayerState(), nextPlayer), game.ludoPlayers[0].GetPlayerState());
+                }
+            }
+        }
     }
     private void InitLudoUI()
     {
@@ -70,16 +102,29 @@ public class LudoController : Game
         terningBtn.clicked += () =>
         {
             System.Random rnd = new System.Random();
-            Debug.Log(rnd.Next(1,7));
-            Debug.Log(rnd.Next(1,7));
-            Debug.Log(CurrentState);
+            int sum = rnd.Next(1,7) + rnd.Next(1,7);
+            Debug.Log(sum);
+            Debug.Log(CurrentState.ToString());
+            LudoPlayer cPlayer = game.ludoPlayers.Where(x => x.GetPlayerState() == CurrentState).First();
+            Debug.Log("SOME PLAYER STUFF: " + cPlayer.GetColor());
+
+            game.MovePiece(-1, cPlayer.GetColor(), sum);
+            Debug.Log("END CALL");
             //move piece
+            
             MoveNext(nextPlayer);
 
         };
         root.Add(terningBtn);
         //MoveNext(nextPlayer);
-
+    }
+    private int AddPlayer()
+    {
+        game.ludoPlayers.Add(new LudoPlayer("DEMO", PlayerColors[game.ludoPlayers.Count + 1], false, new State("player" + (game.ludoPlayers.Count + 1))));
+        //Debug.Log(ludoPlayers[ludoPlayers.Count-1].GetColor());
+        game.CreatePieces(game.ludoPlayers[game.ludoPlayers.Count - 1].GetColor());
+        return game.ludoPlayers.Count;
+        
     }
 }
 
@@ -87,7 +132,7 @@ public class LudoController : Game
 
 public class LudoGame
 {
-    private List<LudoPlayer> ludoPlayers = new List<LudoPlayer>();
+    
     //private Dictionary<int, ludo_piece> ludo_pieces = new Dictionary<int, ludo_piece>();
     private readonly Dictionary<int, string> colors = new Dictionary<int, string>() {
         {1,"Red" },
@@ -96,66 +141,85 @@ public class LudoGame
         {4,"Yellow" }
     };
     private List<LudoPiece> ludoPieces = new List<LudoPiece>();
-
+    public List<LudoPlayer> ludoPlayers = new List<LudoPlayer>();
+    private int ColorNameToKey(string color)
+    {
+        return colors.FirstOrDefault(x => x.Value == color).Key;
+    }
         
     public void StartGame()
     {
         Debug.Log("START GAME");
     }
-    public int AddPlayer(string name, bool aiPlayer) //Returns player ID
+    public int CreatePieces(string color) //Returns player ID
     {
-        if (ludoPlayers.Count < 4)//Check if player count exceeds max count of 4
+        for(int i = 0;i < 4; i++)
         {
-            LudoPlayer newLudoPlayer = new LudoPlayer(name,ludoPlayers.Count + 1, aiPlayer);
-            ludoPlayers.Add(newLudoPlayer);
-            LudoPiece LudoPiece;
-            for (int i = 0; i < 4; i++)
+            ludoPieces.Add(new LudoPiece(-1, ColorNameToKey(color)));
+        }
+        return 0;
+    }
+    public int MovePiece(int absolutepPosition, string color, int amount)
+    {
+        if(absolutepPosition == -1 && amount == 12)
+        {
+            //Move a player piece to relative potition 0
+            Debug.Log("Number of pieces: " + ludoPieces.Count);
+            foreach(LudoPiece piece in ludoPieces)
             {
-                LudoPiece = new LudoPiece(-1, newLudoPlayer.GetColor());
-                ludoPieces.Add(LudoPiece);
-                
-                
+                //Debug.Log(ColorNameToKey(color));
+                if(piece.GetOffset() == ColorNameToKey(color))
+                {
+                    //Debug.Log("START MOVE MABEY");
+                    if(piece.GetAbsolutePosition() == absolutepPosition)
+                    {
+                        piece.SetAbsolutePosition(piece.GetOffset());
+                        Debug.Log("MOVE");
+                        PrintPieces();
+                        return 0;
+                    }
+                }
             }
-            int id = ludoPlayers.Count - 1;
-            return id;//Return the player id
         }
         else
         {
-            return -1;//Return -1 if player counter is 4
+            Debug.Log("No move");
+            return 1;
+        }
+        return -1;
+    }
+    public List<LudoPiece> GetMoveablePieces(string color, int diceSum)
+    {
+
+        return ludoPieces.Where(x => x.GetOffset() == ColorNameToKey(color)).ToList();
+    }
+    public void PrintPieces()
+    {
+        foreach (LudoPiece piece in ludoPieces)
+        {
+            Debug.Log(piece.GetOffset());
+            Debug.Log(piece.GetAbsolutePosition());
         }
     }
-    public LudoPlayer GetPlayer(int playerId)//Returns player object
-    {
-        return ludoPlayers[playerId];
-    }
-    public LudoPlayer RmovePlayer(int playerId)//Returns removed player object
-    {
-        LudoPlayer ludoPlayerToRemove = ludoPlayers[playerId];
-        ludoPlayers.RemoveAt(playerId);
-        return ludoPlayerToRemove;
-    }
 
-    public List<LudoPlayer> GetPlayerList()//Returns a list of type object _ludo_player
-    {
-        return ludoPlayers;
-    }
 
 }
 
 public class LudoPlayer
-{
-    private string name;
-    private int color;
+{     private string name;
+    private string color;
     private bool aiPlayer;
+    private StateMachine.State playerState;
     //private List<LudoPiece> playerPieces = new List<LudoPiece>();
 
-    public LudoPlayer(string name, int color, bool aiPlayer)
+    public LudoPlayer(string name, string color, bool aiPlayer, StateMachine.State playerState)
     {
         this.name = name;
         this.color = color;
         this.aiPlayer = aiPlayer;
+        this.playerState = playerState;
     }
-    public int GetColor()
+    public string GetColor()
     {
         return color;
     }
@@ -163,11 +227,21 @@ public class LudoPlayer
     {
         return name;
     }
-    public void SetColor(int color)
+    public void SetColor(string color)
     {
         this.color=color;
     }
-
+    public StateMachine.State GetPlayerState()
+    {
+        return playerState;
+    }
+    
+    public int SetPlayerState(StateMachine.State playerState)
+    {
+        this.playerState = playerState;
+        return 0;
+    }
+    
 }
 public class LudoPiece
 {
@@ -199,6 +273,10 @@ public class LudoPiece
     {
         this.offset = offset;
         return this.offset;
+    }
+    public int GetRelativePosition()
+    {
+        return absolutepPosition - offset;
     }
 }
 
